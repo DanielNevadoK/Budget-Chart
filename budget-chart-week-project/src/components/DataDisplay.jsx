fb4796ed75d36a86a7bb4a3056deec23516b5bb4
Daniel <daniel.nkroger@gmail.com>
2025-04-01 13:27:35 +0200

    CSS done :)

--- budget-chart-week-project/src/components/DataDisplay.jsx
+++ budget-chart-week-project/src/components/DataDisplay.jsx
@@ -6,15 +6,72 @@ function DataDisplay() {
       <div className="dd-title">
         <h2>Spendig last - 7 days</h2>
       </div>
-      <div className="dd-chart"></div>
+      <div className="dd-chart">
+        <div className="week-day-container">
+            <div className="day">
+                <div className="staple">
+                    
+                </div>
+                <p>Mon</p>
+            </div>
+        </div>
+        <div className="week-day-container">
+            <div className="day">
+                <div className="staple">
+                    
+                </div>
+                <p>Mon</p>
+            </div>
+        </div>
+        <div className="week-day-container">
+            <div className="day">
+                <div className="staple">
+                    
+                </div>
+                <p>Mon</p>
+            </div>
+        </div>
+        <div className="week-day-container">
+            <div className="day">
+                <div className="staple">
+                    
+                </div>
+                <p>Mon</p>
+            </div>
+        </div>
+        <div className="week-day-container">
+            <div className="day">
+                <div className="staple">
+                    
+                </div>
+                <p>Mon</p>
+            </div>
+        </div>
+        <div className="week-day-container">
+            <div className="day">
+                <div className="staple">
+                    
+                </div>
+                <p>Mon</p>
+            </div>
+        </div>
+        <div className="week-day-container">
+            <div className="day">
+                <div className="staple">
+                    
+                </div>
+                <p>Mon</p>
+            </div>
+        </div>
+      </div>
       <div className="dd-summary">
         <div className="dd-sum-text">
           <p>Total this month</p>
           <h1>$478.33</h1>
         </div>
-        <div>
-          <div></div>
-          <div></div>
+        <div className=" dd-sum-text2">
+          <h4>+2.4% </h4>
+          <p>from last month</p>
         </div>
       </div>
     </div>
