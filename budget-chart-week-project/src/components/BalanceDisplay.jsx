ea8490a92746afadb89e667f4e122ec5f4a4bba4
Daniel <daniel.nkroger@gmail.com>
2025-04-02 22:21:14 +0200

    Fixing inputs from user

--- budget-chart-week-project/src/components/BalanceDisplay.jsx
+++ budget-chart-week-project/src/components/BalanceDisplay.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+const calc = function(){
+    const currentBalance = currentBalance - spendings;
+    const spendings = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+
+}
+
+
 function BalanceDisplay() {
   return (
     <div className="balance-cont">
       <div className="balance-title">
         <p>My Balance</p>
-        <h2>$900.2</h2>
+        <h2 id="add-current-balance">$900.2</h2>
       </div>
 
       <div className="logo-cont">
